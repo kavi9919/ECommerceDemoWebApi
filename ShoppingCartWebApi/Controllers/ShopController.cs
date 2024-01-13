f6cd18ebe1f9eddfed341e6e6a10671525362899
Kavishka Nimsara <nimsaragk.21@uom.lk>
2024-01-13 21:07:24 +0530

    update and delete

--- ShoppingCartWebApi/Controllers/ShopController.cs
+++ ShoppingCartWebApi/Controllers/ShopController.cs
@@ -27,9 +27,21 @@ namespace ShoppingCartWebApi.Controllers
 			return await _shopDbContext.Products.ToListAsync();
 		}
 
+		[HttpGet]
+		[Route("GetProduct/{id}")]
+		public async Task<IActionResult> GetStudentById(int id)
+		{
+			var product = await _shopDbContext.Products.FindAsync(id);
 
+			if (product == null)
+			{
+				return NotFound(); // Return 404 Not Found if the student with the specified ID is not found.
+			}
 
-		[HttpPost]
+			return Ok(product); // Return 200 OK with the student information if found.
+		}
+
+			[HttpPost]
 		[Route("PostProduct")]
 		public async Task<Product> AddProduct(Product product)
 		{
@@ -38,7 +50,39 @@ namespace ShoppingCartWebApi.Controllers
 			return product;
 		}
 
+		[HttpPatch]
+		[Route("UpdateProduct/{id}")]
+
+		public async Task<Product> UpdateStudent(Product product)
+		{
+			_shopDbContext.Entry(product).State = EntityState.Modified;
+			await _shopDbContext.SaveChangesAsync();
+			return product;
+		}
+
+		[HttpDelete]
+		[Route("DeleteProduct/{id}")]
+
+		public bool DeleteStudent(int id)
+		{
+			bool a = false;
+			var product = _shopDbContext.Products.Find(id);
+			if (product != null)
+			{
+				a = true;
+				_shopDbContext.Entry(product).State = EntityState.Deleted;
+				_shopDbContext.SaveChanges();
+			}
+			else
+			{
+				a = false;
+			}
+			return a;
+		}
+
 	}
+
+
 
 
 }
